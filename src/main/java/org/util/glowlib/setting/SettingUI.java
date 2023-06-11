62149e69879ebb426d808dc218677d292363091b
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-06-11 18:49:59 -0400

    Added a separate name variable to the SettingUI class to allow for any name while still saving with a unique key

--- src/main/java/org/util/glowlib/setting/SettingUI.java
+++ src/main/java/org/util/glowlib/setting/SettingUI.java
@@ -8,6 +8,7 @@ package org.util.glowlib.setting;
 public class SettingUI<T> extends Setting<T> {
 
 
+    private final String name;
     private final String description;
 
     private Double min;
@@ -21,13 +22,15 @@ public class SettingUI<T> extends Setting<T> {
      * Default Constructor. This is a general constructor which is not specialized and can hold any value which is good
      * for use of booleans, Strings, etc. as they do not require special parameters
      */
-    private SettingUI(SettingManager settingManager, String name, String description, T defaultVal) {
-        super(settingManager,name,defaultVal);
+    private SettingUI(SettingManager settingManager, String key, String name, String description, T defaultVal) {
+        super(settingManager,key,defaultVal);
+        this.name = name;
         this.description = description;
     }
 
-    private SettingUI(String name, String description, T defaultVal) {
-        super(name,defaultVal);
+    private SettingUI(String key, String name, String description, T defaultVal) {
+        super(key,defaultVal);
+        this.name = name;
         this.description = description;
     }
 
@@ -35,15 +38,15 @@ public class SettingUI<T> extends Setting<T> {
      * Setting Number. This is a type of UI setting that has a minimum, maximum, and step value for slider UI systems
      * It is able to contain any number value needed to be saved and/or modified by the UI
      */
-    public SettingUI(SettingManager settingManager, String name, String description, T defaultValue, double min, double max, double step) {
-        this(settingManager,name,description,defaultValue);
+    public SettingUI(SettingManager settingManager, String key, String name, String description, T defaultValue, double min, double max, double step) {
+        this(settingManager,key,name,description,defaultValue);
         this.min = min;
         this.max = max;
         this.step = step;
     }
 
-    public SettingUI(String name, String description, T defaultValue, double min, double max, double step) {
-        this(name,description,defaultValue);
+    public SettingUI(String key, String name, String description, T defaultValue, double min, double max, double step) {
+        this(key,name,description,defaultValue);
         this.min = min;
         this.max = max;
         this.step = step;
@@ -53,17 +56,20 @@ public class SettingUI<T> extends Setting<T> {
      * Setting Mode. This is a type of UI setting that has a defined set of possible values to be cycled through.
      */
     @SafeVarargs
-    public SettingUI(SettingManager settingManager, String name,  String description, T defaultValue, T... modes) {
-        this(settingManager,name,description,defaultValue);
+    public SettingUI(SettingManager settingManager, String key, String name, String description, T defaultValue, T... modes) {
+        this(settingManager,key,name,description,defaultValue);
         this.modes = modes;
     }
 
     @SafeVarargs
-    public SettingUI(String name, String description, T defaultValue, T... modes) {
-        this(name,description,defaultValue);
+    public SettingUI(String key, String name, String description, T defaultValue, T... modes) {
+        this(key,name,description,defaultValue);
         this.modes = modes;
     }
 
+    public String getName() {
+        return name;
+    }
 
     public String getDescription() {
         return description;
