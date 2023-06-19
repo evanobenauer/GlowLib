73fafed0bd41641543951c46d15493228c2178b2
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-06-19 02:29:56 -0400

    Repaired a bug in the SettingManager where if a setting was not found, but tried to be loaded, all settings would fail to load.

--- src/main/java/com/ejo/glowlib/setting/SettingManager.java
+++ src/main/java/com/ejo/glowlib/setting/SettingManager.java
@@ -71,27 +71,33 @@ public class SettingManager {
 
         try {
             for (String[] data : dataList) {
-                Setting setting = getSettingList().get(data[0]);
-                String savedVal = data[1];
-                String datatype = data[2];
+                try {
+                    Setting setting = getSettingList().get(data[0]);
+                    String savedVal = data[1];
+                    String datatype = data[2];
 
-                if (datatype.equals("unloadable")) continue;
+                    if (datatype.equals("unloadable")) continue;
 
-                if (savedVal.equals("null") || datatype.equals("nullType")) setting.set(null);
-                else if (datatype.equals("string")) setting.set(savedVal);
-                else if (datatype.equals("integer")) setting.set(Integer.parseInt(savedVal));
-                else if (datatype.equals("float")) setting.set(Float.parseFloat(savedVal));
-                else if (datatype.equals("double")) setting.set(Double.parseDouble(savedVal));
-                else if (datatype.equals("boolean")) setting.set(Boolean.parseBoolean(savedVal));
-                else if (datatype.equals("datetime")) setting.set(new DateTime(savedVal));
-                else if (datatype.equals("vector")) {
-                    String[] vecVals = savedVal.replace("<","").replace(">","").split("\\|");
-                    setting.set(new Vector(Double.parseDouble(vecVals[0]),Double.parseDouble(vecVals[1]),Double.parseDouble(vecVals[2])));
-                } else if (datatype.equals("colore")) {
-                    String[] vecVals = savedVal.substring(9).replace("]","").split("\\|");
-                    setting.set(new ColorE(Integer.parseInt(vecVals[0]),Integer.parseInt(vecVals[1]),Integer.parseInt(vecVals[2]),Integer.parseInt(vecVals[3])));
+                    if (savedVal.equals("null") || datatype.equals("nullType")) setting.set(null);
+                    else if (datatype.equals("string")) setting.set(savedVal);
+                    else if (datatype.equals("integer")) setting.set(Integer.parseInt(savedVal));
+                    else if (datatype.equals("float")) setting.set(Float.parseFloat(savedVal));
+                    else if (datatype.equals("double")) setting.set(Double.parseDouble(savedVal));
+                    else if (datatype.equals("boolean")) setting.set(Boolean.parseBoolean(savedVal));
+                    else if (datatype.equals("datetime")) setting.set(new DateTime(savedVal));
+                    else if (datatype.equals("vector")) {
+                        String[] vecVals = savedVal.replace("<", "").replace(">", "").split("\\|");
+                        setting.set(new Vector(Double.parseDouble(vecVals[0]), Double.parseDouble(vecVals[1]), Double.parseDouble(vecVals[2])));
+                    } else if (datatype.equals("colore")) {
+                        String[] vecVals = savedVal.substring(9).replace("]", "").split("\\|");
+                        setting.set(new ColorE(Integer.parseInt(vecVals[0]), Integer.parseInt(vecVals[1]), Integer.parseInt(vecVals[2]), Integer.parseInt(vecVals[3])));
+                    }
+                    //If we reach here, the setting will be set to its default value as it is un-readable
+                } catch (Exception e) {
+                    //The specific setting could not be found
+                    e.printStackTrace();
+                    continue;
                 }
-                //If we reach here, the setting will be set to its default value as it is un-readable
             }
             return true;
         } catch (Exception e) {
