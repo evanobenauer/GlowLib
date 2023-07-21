994feba52d59e57a1cddc63702418ce409eb1b21
Evan Obenauer <evanjohnobenauer@gmail.com>
2023-07-21 00:57:25 -0400

    Added ListUtil Added getMaxRowSize and getMaxStringLength to ListUtil

---
+++ src/main/java/com/ejo/glowlib/util/ListUtil.java
@@ -0,0 +1,21 @@
+package com.ejo.glowlib.util;
+
+public class ListUtil {
+
+    public static <T> int getMaxRowSize(T[][] grid) {
+        int length = grid[0].length;
+        for (T[] row : grid) {
+            if (row.length > length) length = row.length;
+        }
+        return length;
+    }
+
+    public static int getMaxStringLength(String[] list) {
+        int length = list[0].length();
+        for (String string : list) {
+            if (string.length() > length) length = string.length();
+        }
+        return length;
+    }
+
+}
